dbbc0244011041fba44a7b057252ede95bf16659
keshavtiwari07 <keshavprasad123459@gmail.com>
2023-10-03 19:57:15 +0530

    added more comments

--- pattern8.cpp
+++ pattern8.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main() {
     int n=5;
 	for(int i=1;i<=n;i++){//for the rows
+	   //for the spaces
 	    for(int j=1;j<=i;j++){
 	        cout<<" ";
-	    } 
-	     for(int j=1;j<=2*(n-i)+1;j++){
+	    }
+		//for the stars 
+	     for(int j=1;j<=2*(n-i)+1;j++){ 
 	        cout<<"*";
 	    }
+		//for the spaces
 	    for(int j=1;j<=i;j++){
 	        cout<<" ";
 	    }
