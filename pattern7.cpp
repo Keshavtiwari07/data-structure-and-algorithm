f8edc22c60417dbb8e53c5d73bac6929f951fd8f
keshavtiwari07 <keshavprasad123459@gmail.com>
2023-12-11 23:38:31 +0530

    details

--- pattern7.cpp
+++ pattern7.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
     int n=5;
-	for(int i=1;i<=n;i++){
-	    for(int j=1;j<=n-i;j++){
+	for(int i=1;i<=n;i++){// for the number of rows
+	    for(int j=1;j<=n-i;j++){// for no. of column of space in start
 	        cout<<" ";
 	    } 
-	     for(int j=1;j<=2*i-1;j++){
+	     for(int j=1;j<=2*i-1;j++){//for no. of column of stars
 	        cout<<"*";
 	    }
-	    for(int j=1;j<=n-i;j++){
+	    for(int j=1;j<=n-i;j++){//for no. of column of space in end
 	        cout<<" ";
 	    }
 	    cout<<endl;
