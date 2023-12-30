9feaa9629948b052fdc8ed7998532df4eff7eec0
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-12-31 00:08:30 +0530

    string output

---
+++ string-output.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+int main() {
+    string st="dgcghrhmc";
+    for (auto kuchbhiword:st){
+        cout<< kuchbhiword<<" ";
+    }
+  //  d g c g h r h m c 
+    
+  return 0;
+}
