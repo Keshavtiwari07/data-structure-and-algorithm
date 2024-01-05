12dd16bb1a55685fa946e7325fdec70cefb7d67b
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2024-01-05 14:17:34 +0530

    .

---
+++ sde-sheet-codingninja/move-zero-to-end.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+vector<int> moveZeros(int n, vector<int> a) {
+    // Write your code here.
+    int j=0;
+    vector<int>vec(n);
+
+    for(int i=0;i<n;i++){
+        if (a[i]!=0){
+          vec[j] = a[i] ;
+          j++;
+        }
+    }
+
+    return vec;
+ 
+  
+}
+
+
