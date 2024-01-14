43837fb93e8af6bda0f1b004c94bb12d119a3ae8
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2024-01-14 23:43:50 +0530

    comments added

--- Recursion /factorial.cpp
+++ Recursion /factorial.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
- 
- int fact(int n){
-     if (n==0) return 1;
-     return    n * fact (n-1);  
-     
- }
 
+// Recursive function to calculate the factorial of a number 'n'
+int fact(int n) {
+    // Base case: if n is 0, factorial is 1
+    if (n == 0)
+        return 1;
+
+    // Recursive case: return the product of n and the factorial of (n-1)
+    return n * fact(n - 1);
+}
 
 int main() {
     int n;
-    cin>>n;
-    cout <<fact(n);
 
-	return 0;
+    // Input: Get the value of n from the user
+    cin >> n;
+
+    // Output: Display the factorial of n using the fact function
+    cout << fact(n);
+
+    return 0;
 }
